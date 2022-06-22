cae966de11dc29f7d5ec93a177596a42765e81f2
ismailraju <ismailhossainraju74@gmail.com>
2022-06-22 09:46:14 +0600

    first commit

---
+++ servletDemo/src/net/jcip/examples/CachedFactorizer.java
@@ -0,0 +1,72 @@
+package net.jcip.examples;
+
+import java.io.IOException;
+import java.math.BigInteger;
+import javax.servlet.*;
+
+import net.jcip.annotations.*;
+
+/**
+ * CachedFactorizer
+ * <p/>
+ * Servlet that caches its last request and result
+ *
+ * @author Brian Goetz and Tim Peierls
+ */
+@ThreadSafe
+public class CachedFactorizer extends GenericServlet implements Servlet {
+    @GuardedBy("this")
+    private BigInteger lastNumber;
+    @GuardedBy("this")
+    private BigInteger[] lastFactors;
+    @GuardedBy("this")
+    private long hits;
+    @GuardedBy("this")
+    private long cacheHits;
+
+    public synchronized long getHits() {
+        return hits;
+    }
+
+    public synchronized double getCacheHitRatio() {
+        return (double) cacheHits / (double) hits;
+    }
+
+    public void service(ServletRequest req, ServletResponse resp) {
+        BigInteger i = extractFromRequest(req);
+        BigInteger[] factors = null;
+        synchronized (this) {
+            ++hits;
+            if (i.equals(lastNumber)) {
+                ++cacheHits;
+                factors = lastFactors.clone();
+            }
+        }
+        if (factors == null) {
+            factors = factor(i);
+            synchronized (this) {
+                lastNumber = i;
+                lastFactors = factors.clone();
+            }
+        }
+        encodeIntoResponse(resp, factors);
+    }
+
+    void encodeIntoResponse(ServletResponse resp, BigInteger[] factors) {
+        resp.setContentType("text/html");
+        try {
+            resp.getWriter().write("<h1>raju</h1>");
+        } catch (IOException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+
+    BigInteger extractFromRequest(ServletRequest req) {
+        return new BigInteger("7");
+    }
+
+    BigInteger[] factor(BigInteger i) {
+        // Doesn't really factor
+        return new BigInteger[]{i};
+    }
+}
